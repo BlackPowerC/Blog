bcd9171561808faf586ad23f458f96c55d61047d
fatigba <fatigba72@gmail.com>
2017-12-25 05:47:25 +0000

    Suppression de tout le code de traitement.

--- src/app/post/post_connexion.php
+++ src/app/post/post_connexion.php
@@ -10,46 +10,7 @@ if (isset($_GET['action']))
         case "login":
             if (isset($_POST['pseudo']) AND isset($_POST['passe']))
             {
-                $i = 0;
-                $pseudo = strip_tags($_POST['pseudo']);
-                $passe = strip_tags($_POST['passe']);
-                $requete = Database::getInstance()->prepare("SELECT * FROM user WHERE pseudo = :user_pseudo AND passe = :user_passe");
-                $requete->execute(array("user_passe" => $passe, "user_pseudo" => $pseudo));
-                $reponse = $requete->fetch(PDO::FETCH_ASSOC);
-                $requete->closeCursor();
-                foreach ($reponse as $value)
-                {
-                    $i++;
-                    if ($i)
-                    {
-                        break;
-                    }
-                }
-                if ($i == 0)
-                {
-                    /* La base de données de contient aucune données correspondante */
-                    /* Redirection vers une page d'inscription */
-                    //	echo "pas de bol<br/>" ;
-                    header("Location: ../frontend/inscription.php?msg=bad_action");
-                    exit();
-                } else if ($i == 1)
-                {
-                    $_SESSION['passe'] = $passe;
-                    $_SESSION['pseudo'] = $pseudo;
-                    $_SESSION['id'] = (integer) $reponse['id'];
-                    $_SESSION['pays'] = $reponse['pays'];
-                    $_SESSION['age'] = (integer) $reponse['age'];
-                    $_SESSION['sexe'] = $reponse['sexe'];
-                    $_SESSION['id_type'] = (integer) $reponse['id_type'];
-                    $_SESSION['token'] = sha1($passe . $pseudo);
-                    $_SESSION['time'] = time() ;
-                    if (isset($_POST['remind']))
-                    {
-                        $_SESSION['persist'] = ( (isset($_POST['remind']) AND strcmp($_POST['remind'], 'on') == 0 ) ) ? true : false;
-                    }
-                    header('Location: http://' . $_SERVER["SERVER_NAME"] . strip_tags($_GET['uri']));
-                    exit();
-                }
+                
             }
             break;
         case "logout":
