70bdb453a11253379813906641fe96510f1d260f
fatigba <fatigba72@gmail.com>
2017-12-25 05:46:41 +0000

    Légère amélioration.

--- src/app/post/post_vote.php
+++ src/app/post/post_vote.php
@@ -8,19 +8,17 @@
 
 session_start();
 
-require_once '../classe/Database.php';
 require_once '../classe/Vote.php';
 
 if (isset($_SESSION["token"]))
 {
     if (isset($_POST["vote"]) AND isset($_POST["id_article"]))
     {
-        $vote = (int) strip_tags($_POST["vote"]);
+        $vote = boolval((int) strip_tags($_POST["vote"]));
         $id_article = (int) strip_tags($_POST["id_article"]);
         
         $this_vote = new Vote(["id_user"=>$_SESSION["id"], "id_article"=>$id_article, "type_vote"=>$vote]) ;
         $this_vote->vote() ;
-        
     }
 }
 /* redirection en envoyant un message d'erreur */
