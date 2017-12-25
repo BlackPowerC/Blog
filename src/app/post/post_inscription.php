9efa2210ebd0fd0a77a135d69d7c6a7105401bbf
fatigba <fatigba72@gmail.com>
2017-12-25 05:47:15 +0000

    Suppression de tout le code de traitement.

--- src/app/post/post_inscription.php
+++ src/app/post/post_inscription.php
@@ -1,28 +1,11 @@
 <?php
-
 session_start();
-
-function c_striptags(array $array)
-{
-    foreach ($array as $key => $value)
-    {
-        $value = strip_tags($value);
-    }
-}
 
 if (isset($_POST['passe']) AND isset($_POST['pseudo']) AND isset($_POST['sexe']))
 {
     require_once '../classe/User.php';
-    require_once '../classe/Database.php';
 
-    c_striptags($_POST);
-    $user = new User($_POST);
-    $sql_statement = "INSERT INTO user VALUES(NULL, 1, :pseudo, :passe, :age, :sexe, :pays)";
-    $param = array("pseudo" => $user->getPseudo(), "passe" => $user->getPasse(), "sexe" => $user->getSexe()
-        , "age" => $user->getAge(), "pays" => $user->getPays());
-    $request = Database::getInstance()->prepare($sql_statement);
-    $request->execute($param);
-    $request->closeCursor();
+    
 
     /* Redirection vers la page de connexion */
     header("Location: ../frontend/connexion.php") ;
