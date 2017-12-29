868ac660e6ceed0d2902a14ab2446d44e96de47e
fatigba72 <fatigba72@gmail.com>
2017-12-29 09:23:38 +0000

    post_connexion.php: inclusion de session_helper et UserManager

--- src/app/post/post_connexion.php
+++ src/app/post/post_connexion.php
@@ -2,19 +2,21 @@
 
 session_start();
 require_once '../classe/Database.php';
+require_once '../helper/session_helper.php';
+require_once '../repository/manager/UserManager.php' ;
 
 if (isset($_GET['action']))
 {
     switch (strip_tags($_GET['action']))
     {
         case "login":
-            if (isset($_POST['pseudo']) AND isset($_POST['passe']))
+            if (isset($_POST['pseudo']) AND isset($_POST['email']))
             {
                 
             }
             break;
         case "logout":
-            session_destroy();
+            session_end();
             header('Location: http://' . $_SERVER["SERVER_NAME"] . '/' . strip_tags($_GET['uri']));
             exit();
             break;
