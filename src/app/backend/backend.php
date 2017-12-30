2de6403e24eb5a7115c3f497a2a06f258cf432df
fatigba72 <fatigba72@gmail.com>
2017-12-30 12:31:02 +0000

    backend.php: autoloading et correction de requête SQL et utilisation de ArticleManager.

--- src/app/backend/backend.php
+++ src/app/backend/backend.php
@@ -2,12 +2,16 @@
 
 session_start();
 
-require_once '../classe/Article.php';
-require_once '../classe/Database.php';
+require_once '../core/Autoloader.php';
+Autoloader::getInstance()->load_class("database") ;
+Autoloader::getInstance()->load_entity("article") ;
+Autoloader::getInstance()->load_manager("articleManager") ;
 
+//require_once '../classe/Article.php';
+//require_once '../classe/Database.php';
+        
 class backendController
 {
-
     public function __construct()
     {
         
@@ -20,21 +24,13 @@ class backendController
 
     public function redigerAction()
     {
-        $sql_query = "SELECT a.id, a.date, a.titre, a.text, a.id_user
-                        FROM article a 
-                        LEFT JOIN user u ON u.id = a.id_user
-                        WHERE u.id=:id_user" ;
-        
-        $requete = Database::getInstance()->prepare($sql_query);
-        $requete->execute(array("id_user"=>$_SESSION['id'])) ;
-        $articles = $requete->fetchAll(PDO::FETCH_ASSOC);
+        $articles = ArticleManager::getInstance()->findAll() ;
         include_once '../views/backend/backend_redigerView.php';
     }
-
 }
 
 $bc = new backendController();
-if (isset($_SESSION['id_type']) AND $_SESSION['id_type'] == 2)
+if (isset($_SESSION['id']) AND $_SESSION['id'] == 1)
 {
     if (isset($_GET['link']))
     {
