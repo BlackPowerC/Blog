72ae87e3f46777e2391c71b22756d869009c100c
fatigba72 <fatigba72@gmail.com>
2017-12-30 12:11:11 +0000

    Réimplémentation de la fonctionalité d'inscription avec Login et Email, avec utilisation de User et UserManager. Utilisation de l'autoloader.

--- src/app/post/post_inscription.php
+++ src/app/post/post_inscription.php
@@ -1,14 +1,31 @@
 <?php
 session_start();
 
-if (isset($_POST['passe']) AND isset($_POST['pseudo']) AND isset($_POST['sexe']))
+if (isset($_POST['email']) AND isset($_POST['pseudo']))
 {
-    require_once '../classe/User.php';
-
+    require_once '../core/Autoloader.php';
+    Autoloader::getInstance()->load_entity('user') ;
+    Autoloader::getInstance()->load_manager('userManager') ;
+    Autoloader::getInstance()->load_class('database') ;
     
-
-    /* Redirection vers la page de connexion */
-    header("Location: ../frontend/connexion.php") ;
+    $user = new User();
+    $user->setEmail(strip_tags($_POST['email']));
+    $user->setPseudo(strip_tags($_POST['pseudo']));
+//    if(preg_match("[A-Za-z0-9]{6, 20}$", $_POST['pseudo']===1) 
+//            && preg_match("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9]+\.[a-zA-Z0-9-.]+$", $_POST['email'])===1)
+//    {
+        $result = UserManager::getInstance()->get($user) ;
+        /* Utilisateur non inscrit */
+        if(is_null($result))
+        {
+            UserManager::getInstance()->insert($user);
+            /* Allez vers la page de connexion */
+            header("Location: ../frontend/connexion.php?msg=good_action") ;
+            exit() ;
+//        }
+    }
+    /* Utilisateur inscrit */
+    header("Location: ../frontend/connexion.php?msg=bad_action") ;
     exit() ;
 }
 else
