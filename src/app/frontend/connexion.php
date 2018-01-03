8d092b357451ab79f8384b7782c67c409dbc095a
fatigba72 <fatigba72@gmail.com>
2018-01-03 20:42:32 +0000

    Amélioration. J'ai ajouté des lignes pour donner des messages à l'utilisateur. Je n'oublie pas aussi l'inclusion de form.inc.php pour le formulaire de connexion.

--- src/app/frontend/connexion.php
+++ src/app/frontend/connexion.php
@@ -22,46 +22,36 @@ Autoloader::getInstance()->load_class('pages') ;
             <div class="text-center">
                 <h1>Connexion</h1>
             </div>
+            <div class="row" style="margin-bottom: 25px;">
+                <?php
+                if (isset($_GET["msg"]))
+                {
+                    $msg = strip_tags($_GET["msg"]);
+                    switch ($msg)
+                    {
+                        case 'bad_action':
+                            ?>
+                            <div class="alert alert-danger">
+                                Vous êtes déjà inscrit, veuillez vous connecter !
+                            </div>
+                            <?php
+                            break;
+                        case 'good_action':
+                            ?>
+                            <div class="alert alert-info">
+                                Connecte-toi sur cette page !
+                            </div>	
+                            <?php
+                            break;
+                    }
+                }
+                ?>
+            </div>
             <div class="row">
-                <?php echo form("../post/post_connexion.php?action=login&uri={$_SERVER["REQUEST_URI"]}", "POST") ?>
-                <div class="form-group">
-                    <?php
-                    echo form_label("Pseudo", "pseudo");
-                    echo form_input([
-                        "type" => "text",
-                        "class" => "form-control",
-                        "placeholder" => "Saisir pseudo",
-                        "name" => "pseudo",
-                        "required" => ""
-                    ]);
-                    ?>
-                </div>
-                <!-- Email -->
-                <div class="form-group">
-                    <?php
-                    echo form_label("Email", "email");
-                    echo form_input([
-                        "type" => "e-mail",
-                        "class" => "form-control",
-                        "placeholder" => "Saisir adresse électonique",
-                        "name" => "email",
-                        "required" => ""
-                    ]);?>
-                </div>
-                <div class="checkbox">
-                    <label for="remind"><?php echo form_input([
-                        "class"=>"checkbox",
-                        "name"=>"remind",
-                        "type"=>"checkbox"
-                        ])?>Se souvenir de moi</label>
-                </div>'
-                <div class="form_control_content" style="width: 200px; margin: auto">
-                    <?php
-                    echo form_input(["type" => "submit", "class" => "btn btn-primary"]);
-                    echo form_input(["type" => "reset", "class" => "btn btn-primary"]);
-                    ?>
-                </div>
-                </form>
+            <?php
+                $action="../post/post_connexion.php?action=login&uri=".$_SERVER["REQUEST_URI"] ;
+                include_once '../views/form.inc.php';
+            ?>
             </div>
         </div>
     </body>
